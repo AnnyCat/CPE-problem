d0fc50ae6f757fa6db54dd67f71c1764abda7af6
anny20323 <anny20323@gmail.com>
2020-05-15 23:13:59 +0800

    add 11006

---
+++ 11006_Rank the Languages.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main(){
+
+    int n,world_num=1;
+    cin>>n;
+    while(n--){
+        int H,W;
+        cin>>H>>W;
+        int total=0;
+        int record[26]={0};//a-z
+        char matrix[H][W];
+        int Color[H*W]={0};
+        int Distance[H*W]={0};
+        vector <int> AdjList [H*W];//store index
+        queue <int> Queue;
+        for(int i=0;i<H;i++)
+            for(int j=0;j<W;j++)
+                cin>>matrix[i][j];
+        // Create Adjacency List
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W;j++){
+                if(i!=0){//not at top side
+                    if(matrix[i][j]==matrix[i-1][j])
+                        AdjList[i*W+j].push_back((i-1)*W+j);
+                }
+                if(j!=0){//not at left side
+                    if(matrix[i][j]==matrix[i][j-1])
+                        AdjList[i*W+j].push_back(i*W+j-1);
+                }
+                if(i!=(H-1)){//not at bottom side
+                    if(matrix[i][j]==matrix[i+1][j])
+                        AdjList[i*W+j].push_back((i+1)*W+j);
+                }
+                if(j!=(W-1)){//not at right side
+                    if(matrix[i][j]==matrix[i][j+1])
+                        AdjList[i*W+j].push_back(i*W+j+1);
+                }
+
+            }
+        }
+        //BFS
+        int i=0;//start
+        for(int j=0;j<H*W;j++){
+            if(Color[i]==0){//start point
+                Color[i] = 1;
+                Distance[i]=0;
+                Queue.push(i);
+                total++;
+                while(!Queue.empty()){
+                    int f = Queue.front();
+                    for(int k=0;k<AdjList[f].size();k++){//k=f's neighbor
+                        if(Color[AdjList[f].at(k)]==0){
+                            Queue.push(AdjList[f].at(k));
+                            Color[AdjList[f].at(k)]=1;
+                            Distance[AdjList[f].at(k)] = Distance[f] + 1;
+                        }
+                    }
+                    Queue.pop();
+                }
+            }
+            i=j;
+        }
+        for(int j=0;j<H*W;j++){
+            if(Distance[j]==0){
+                int index = matrix[j/W][j%W]-'a';
+                record[index]++;
+            }
+        }
+
+        cout<<"World #"<<world_num++<<endl;
+        for(int i=total;i>=1;i--){
+            for(int j=0;j<26;j++){
+                if(record[j]==i)
+                    cout<<char(j+'a')<<": "<<i<<endl;
+            }
+        }
+
+
+    }
+
+
+
+    return 0;
+}
+
+
